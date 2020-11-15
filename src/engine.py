f64e937cbae872cc34c57c7066673cb54b9399ca
guillaume <guillaume_cummings@hotmail.com>
2020-11-15 18:16:32 -0500

    change game

--- src/engine.py
+++ src/engine.py
@@ -4,7 +4,7 @@ from agents.sarsaAgent import SarsaAgent, SarsaExperiment
 from agents.reinforce import REINFORCEAgent, ReinforceExperiment
 
 def run(algo):
-    env = gym.make('Frostbite-ram-v0')
+    env = gym.make('SpaceInvaders-ram-v0')
 
     if algo == 'sarsa':
         agent = SarsaAgent(env.observation_space, env.action_space, epsilon=0.1, alpha=0.01, gamma=0.1)
@@ -15,9 +15,9 @@ def run(algo):
             actions_space=env.action_space,
             learning_rate = 0.001,
             gamma = 0.99,
-            hidden1 = 16,
-            hidden2 = 16,
-            hidden3 = 8)
+            hidden1 = 128,
+            hidden2 = 18,
+            hidden3 = 18)
         ReinforceExperiment(env,agent, stop_criterion=10000, EPISODES=100000).run()
 
 run('reinforce')
