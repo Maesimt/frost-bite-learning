226588275bf016cee6734ee9e04b532cdfbab152
guillaume <guillaume_cummings@hotmail.com>
2020-11-15 14:37:19 -0500

    new arch

--- src/engine.py
+++ src/engine.py
@@ -8,15 +8,15 @@ def run(algo):
 
     if algo == 'sarsa':
         agent = SarsaAgent(env.observation_space, env.action_space, epsilon=0.1, alpha=0.01, gamma=0.1)
-        SarsaExperiment().run(agent, env, 1000)
+        SarsaExperiment().run(agent, env, 100000)
     elif algo == 'reinforce':
         agent = REINFORCEAgent(
             observation_space=env.observation_space,
             actions_space=env.action_space,
-            learning_rate = 0.001,
-            gamma = 0.99,
-            hidden1 = 36,
-            hidden2 = 36)
+            learning_rate = 0.01,
+            gamma = 0.9,
+            hidden1 = 128,
+            hidden2 = 18)
         ReinforceExperiment(env,agent, stop_criterion=1000).run()
 
 run('reinforce')
