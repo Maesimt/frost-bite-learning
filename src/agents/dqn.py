b187278be754f53a29d742c8e917beb94c8a4ce5
guillaume <guillaume_cummings@hotmail.com>
2020-11-23 21:02:06 -0500

    fix error

--- src/agents/dqn.py
+++ src/agents/dqn.py
@@ -81,9 +81,9 @@ class DQNAgent(Agent):
         # decay epsilon after each epoch
         if self.epsilon_decay:
             if self.step_counter % self.epoch_length == 0:
-                if self.average_score > 400
+                if self.average_score > 400:
                     self.epsilon = max(.01, self.epsilon * .9995)
-                else
+                else:
                     self.epsilon = max(.2, self.epsilon * .9995)
    
         return i
